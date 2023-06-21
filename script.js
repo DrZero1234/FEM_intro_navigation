5d5485f36720fb2c789b5954d01986d5ae6c2bbd
M.Kristóf <drzero123456@gmail.com>
2023-06-21 20:49:43 +0200

    header done

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const FEATURE_CONTAINER = document.getElementById("features-container");
 const COMPANY_CONTAINER = document.getElementById("company-container");
 const HAMBURGER_WRAPPER = document.getElementById("hamburger-wrapper");
+const SIDENAV = document.getElementById("sidenav");
+const CLOSENAV = document.getElementById("closenav");
+const HEADER_LINKS = document.querySelector(".header-links")
+const SIDENAV_OVERLAY = document.getElementById("sidenav-overlay")
 
 HAMBURGER_WRAPPER.addEventListener("click", () => {
+    const visibility = HEADER_LINKS.getAttribute("data-visible");
+    console.log(visibility)
+    if (visibility === "false") {
+        HEADER_LINKS.setAttribute("data-visible","true")
+        HAMBURGER_WRAPPER.setAttribute("aria-expanded","true")
+        SIDENAV_OVERLAY.style.display = "block"
+    } else {
+        HEADER_LINKS.setAttribute("data-visible","false")
+        HAMBURGER_WRAPPER.setAttribute("aria-expanded","false")
+        SIDENAV_OVERLAY.style.display = "none"
+    }
+    console.log(HAMBURGER_WRAPPER.getAttribute("aria-expanded"))
     HAMBURGER_WRAPPER.classList.toggle("open")
 })
 
@@ -34,12 +50,26 @@ const toggle_dropdown = (e) => {
 
 }
 
+const SMALL_SIZE_ADAPT = (x) => {
+    if (x.matches) {
+        HEADER_LINKS.setAttribute("data-visible", "false")
+    } else {
+        HEADER_LINKS.setAttribute("data-visible", "true")
+        HAMBURGER_WRAPPER.setAttribute("aria-expanded", "false")
+        SIDENAV_OVERLAY.style.display = "none"
+    }
+}
+
 
 [FEATURE_CONTAINER,COMPANY_CONTAINER].forEach((container) => {
     container.addEventListener("click", (e) => {
         toggle_dropdown(e)
     })
 })
+
+var x = window.matchMedia("(max-width: 55em)")
+SMALL_SIZE_ADAPT(x);
+x.addListener(SMALL_SIZE_ADAPT)
 
 add_arrows()
 
